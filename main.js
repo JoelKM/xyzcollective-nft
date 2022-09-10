52fc3a5222df12329e81fee0c4a0a617a4ae67ff
JoelKM <joelkasmor16@gmail.com>
2022-09-11 01:02:46 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+
+
+//NAVIGATE ON CLICK
+document.querySelectorAll('nav li').forEach((listitem)=>{
+    listitem.addEventListener('click', ()=>{
+        console.log("click");
+        var topPosition=document.getElementById(listitem.dataset.page).offsetTop-120;
+        window.scrollTo({
+            top: topPosition,
+            left: 0,
+            behavior: 'smooth'
+        })
+    });
+})
